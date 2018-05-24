531659f07b5f8821272a17bb8062c980e03283d1
mbaker7@zagmail.gonzaga.edu <mbaker7@zagmail.gonzaga.edu>
2018-05-23 18:43:08 -0700

    Placeholder UI and minimal Button Function

---
+++ BattleAccountant/Assets/Scripts/TransactionManage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransactionManage : MonoBehaviour {
+
+    public GameObject cashDisplay;
+    private int cash = 1500;
+
+    public void MakeMoney()
+    {
+        cash += 100;
+        cashDisplay.GetComponent<Text>().text = "Money: " + cash;
+    }
+
+}
