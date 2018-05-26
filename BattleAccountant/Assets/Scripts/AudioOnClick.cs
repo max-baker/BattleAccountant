d1ba2c9120245532193b8666bfbab2c231af1a98
mbaker7@zagmail.gonzaga.edu <mbaker7@zagmail.gonzaga.edu>
2018-05-25 21:50:12 -0700

    Added Initial Sound

---
+++ BattleAccountant/Assets/Scripts/AudioOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioOnClick : MonoBehaviour {
+    public AudioClip sound;
+
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start () {  
+        if (source == null)
+        {
+            source = gameObject.AddComponent<AudioSource>();
+            source.clip = sound;
+        }
+        gameObject.GetComponent<Button>().onClick.AddListener(PlaySound);
+    }
+
+    public void PlaySound()
+    {
+        source.PlayOneShot(sound);
+    }
+	
+	
+}
